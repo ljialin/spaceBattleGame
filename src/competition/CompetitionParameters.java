015dfe295c4a0c136569ed61c09402d176528311
Jialin Liu <jialin.liu.cn@gmail.com>
2016-10-07 12:20:31 +0100

    play with params

--- src/competition/CompetitionParameters.java
+++ src/competition/CompetitionParameters.java
@@ -14,7 +14,7 @@ public class CompetitionParameters
   /**
    * Milliseconds allowed per controller action.
    */
-  public static int ACTION_TIME = 40;
+  public static int ACTION_TIME = 10;
 
   /**
    * Milliseconds for controller disqualification, if it returns an action after this time.
