7851fb5011d77356331393b23c75ebe7c2d60f99
SimonLucas <simonmarklucas@gmail.com>
2017-05-12 14:59:24 +0100

    Added tests to time copying game state and performing a game tick

---
+++ src/test/GameCopyTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import core.game.StateObservationMulti;
+import tools.ElapsedTimer;
+
+/**
+ * Created by sml on 12/05/2017.
+ *
+ *  Copying a game state is an important part of simulation-based search
+ *
+ *  Here we're just checking how long it takes
+ *
+ */
+
+public class GameCopyTest {
+
+    public static void main(String[] args) {
+
+        ElapsedTimer timer = new ElapsedTimer();
+        double gameTick = 40;
+
+        int nCopies = (int) 1e6;
+
+        StateObservationMulti game = new StateObservationMulti(false);
+
+        for (int i=0; i<nCopies; i++) {
+
+            game = game.copy();
+
+        }
+
+        System.out.format("Made %d copies\n", nCopies);
+
+        System.out.format("Copies per milli-second: %.1f\n\n", nCopies / (double) timer.elapsed());
+        System.out.format("Game tick time = %d ms\n",(int) gameTick);
+        System.out.format("Copies per milli-second: %d\n\n", (int) (gameTick * nCopies / (double) timer.elapsed()));
+
+        System.out.println(timer);
+
+    }
+}
---
+++ src/test/GameTickTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import core.game.StateObservationMulti;
+import ontology.Types;
+import tools.ElapsedTimer;
+
+/**
+ * Created by sml on 12/05/2017.
+ */
+public class GameTickTest {
+
+    public static void main(String[] args) {
+
+        ElapsedTimer timer = new ElapsedTimer();
+
+        int nTicks = 500;
+        int nTrials = 10000;
+
+        for (int i = 0; i < nTrials; i++) {
+
+            StateObservationMulti game = new StateObservationMulti(false);
+
+            controllers.sampleRandom.Agent randomAgent = new controllers.sampleRandom.Agent(game, null, 0);
+
+            // do not need separate agents for player one and plater 2
+            // since actions are simply random
+
+            for (int j=0; j<nTicks; j++) {
+                Types.ACTIONS a1 = randomAgent.act(game, null);
+                Types.ACTIONS a2 = randomAgent.act(game, null);
+
+                Types.ACTIONS[] actions = new Types.ACTIONS[]{a1, a2};
+                game.advance(actions);
+
+                // record actions made
+
+                // record FEATURES of game state
+
+            }
+            // System.out.println(i + "\t " + game.getGameTick());
+
+        }
+
+        int totalTicks = nTicks * nTrials;
+
+        System.out.format("Made %d ticks\n", totalTicks);
+
+        System.out.format("Ticks per milli-second: %.1f\n\n", totalTicks / (double) timer.elapsed());
+//        System.out.format("Game tick time = %d ms\n", (int) gameTick);
+//        System.out.format("Copies per milli-second: %d\n\n", (int) (gameTick * nCopies / (double) timer.elapsed()));
+
+        System.out.println(timer);
+
+    }
+}
